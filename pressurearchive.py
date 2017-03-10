cded402fd3f23bcb80cebe24562232399c0e3257
Meteodeep <dobg1.91@gmail.com>
2017-03-10 17:44:25 +0000

    Air Pressure Archive

---
+++ pressurearchive.py
@@ -0,0 +1,20 @@
+from time import sleep
+from sense_hat import SenseHat
+
+sense = SenseHat()
+sense.clear()
+
+pressure = sense.get_pressure()
+pressure = round(pressure, 1)
+pressure = str(pressure)
+	
+target1 = open('/var/www/meteodeep/pressurearchiveddata.txt', 'w')
+target1.write(pressure)
+target1.close()
+
+from subprocess import call
+Upload = "/home/pi/Dropbox-Uploader/dropbox_uploader.sh upload /var/www/meteodeep/pressurearchiveddata.txt /PressureArchivedData.txt"
+call ([Upload], shell=True)
+
+# crontab
+# 0 * * * * sudo /usr/bin/python3 /var/www/meteodeep/LivePressureArchive.py
