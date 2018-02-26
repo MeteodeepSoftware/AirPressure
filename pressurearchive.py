0afdcadc3d9f0ce88f06353f1908db95ff940fc9
Meteodeep <dobg1.91@gmail.com>
2018-02-26 23:16:01 +0000

    Update pressurearchive.py

--- pressurearchive.py
+++ pressurearchive.py
@@ -1,6 +1,8 @@
 from time import sleep
 from sense_hat import SenseHat
 
+myAPI = ""
+
 sense = SenseHat()
 sense.clear()
 
@@ -11,6 +13,11 @@ pressure = str(pressure)
 target1 = open('/var/www/meteodeep/pressurearchiveddata.txt', 'w')
 target1.write(pressure)
 target1.close()
+
+baseURL = 'https://api.thingspeak.com/update?api_key=%s' % myAPI  
+
+f = urlopen(baseURL + "&field1=%s" % (str(pressure)))
+f.close() 
 
 from subprocess import call
 Upload = "/home/pi/Dropbox-Uploader/dropbox_uploader.sh upload /var/www/meteodeep/pressurearchiveddata.txt /PressureArchivedData.txt"
